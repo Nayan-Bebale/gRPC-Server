118be53b7fc5a927fdeb3742194871c96bdca71f
Nayan Bebale <nayanbebale2003@Gmail.com>
2025-02-06 10:49:59 +0530

    Update detect.py

--- detect.py
+++ detect.py
@@ -14,7 +14,8 @@ from torchvision.models.detection import (
     KeypointRCNN_ResNet50_FPN_Weights,
     RetinaNet_ResNet50_FPN_Weights,
     SSD300_VGG16_Weights,
-    SSDLite320_MobileNet_V3_Large_Weights
+    SSDLite320_MobileNet_V3_Large_Weights,
+    FCOS_ResNet50_FPN_Weights
 )
 
 # tensorflow
@@ -27,6 +28,9 @@ import matplotlib.pyplot as plt
 import requests
 from PIL import Image
 from io import BytesIO
+
+# Import segmentation Models
+from segmentation_models import run_segmentation
 
 # COCO class labels
 COCO_INSTANCE_CATEGORY_NAMES = [
@@ -67,7 +71,7 @@ def calculate_energy(power_watts, latency_seconds):
     return power_watts * latency_seconds  # Energy in joules (watts * seconds)
 
 
-# Function to load the specified model
+# TorchVision models
 def load_model(model_name="fasterrcnn"):
     if model_name == "fasterrcnn":
         model = torchvision.models.detection.fasterrcnn_resnet50_fpn(weights=FasterRCNN_ResNet50_FPN_Weights.COCO_V1)
@@ -87,10 +91,25 @@ def load_model(model_name="fasterrcnn"):
         model = torchvision.models.detection.ssd300_vgg16(weights=SSD300_VGG16_Weights.COCO_V1)
     elif model_name == "ssdlite":
         model = torchvision.models.detection.ssdlite320_mobilenet_v3_large(weights=SSDLite320_MobileNet_V3_Large_Weights.COCO_V1)
+    elif model_name == "resnet18_detection":
+        model = torchvision.models.detection.retinanet_resnet50_fpn(weights=RetinaNet_ResNet50_FPN_Weights.COCO_V1)
+    elif model_name == "fcos_resnet50":
+        model = torchvision.models.detection.fcos_resnet50_fpn(weights="DEFAULT")
+    elif model_name == "fasterrcnn_inceptionv2":
+        model = torchvision.models.detection.fasterrcnn_resnet50_fpn(weights=FasterRCNN_ResNet50_FPN_Weights.DEFAULT)
+    elif model_name == "maskrcnn_inceptionv2":
+        model = torchvision.models.detection.maskrcnn_resnet50_fpn(weights=MaskRCNN_ResNet50_FPN_Weights.DEFAULT)
+    
+    elif model_name == "ssd_mobilenet_v1":
+        model = torchvision.models.detection.ssdlite320_mobilenet_v3_large(weights=SSDLite320_MobileNet_V3_Large_Weights.DEFAULT)
+    elif model_name == "ssd_resnet34":
+        model = torchvision.models.detection.ssd300_vgg16(weights=SSD300_VGG16_Weights.DEFAULT)
+    
     else:
         raise ValueError(f"Unsupported model: {model_name}")
-    model.eval()  # Set the model to evaluation mode
+    model.eval()
     return model
+
 
 # Function to load TensorFlow models
 def load_tf_model(model_name):
@@ -120,26 +139,142 @@ def load_tf_model(model_name):
     # RetinaNet models
     elif model_name == "retinanet":
         model = hub.load("https://tfhub.dev/tensorflow/retinanet/resnet50_v1_fpn_640x640/1")
-        
+  
     # CenterNet models
     elif model_name == "centernet_hourglass":
         model = hub.load("https://tfhub.dev/tensorflow/centernet/hourglass_512x512/1")
     elif model_name == "centernet_resnet50":
         model = hub.load("https://tfhub.dev/tensorflow/centernet/resnet50v1_fpn_512x512/1")
-        
-    # Mask R-CNN models
-    elif model_name == "mask_rcnn_resnet50":
-        model = hub.load("https://tfhub.dev/tensorflow/mask_rcnn/resnet50_v1_fpn_1024x1024/1")
-    elif model_name == "mask_rcnn_inception":
-        model = hub.load("https://tfhub.dev/tensorflow/mask_rcnn/inception_resnet_v2_atrous_coco/1")
     
-    # Option for other models (e.g., YOLO, OpenPose, etc.)
-    elif model_name == "yolo_v4":
-        model = hub.load("https://tfhub.dev/tensorflow/yolov4-tiny/1")
     else:
         raise ValueError("Model name not recognized. Please choose a valid model.")
     
     return model
+
+
+def load_yo_model(model_name):
+    if model_name == "yolov5n":
+        model = YOLO("yolov5s.pt")
+    elif model_name == "yolov5s":
+        model = YOLO('yolov5s.pt')
+    elif model_name == "yolov5m":
+        model = YOLO('yolov5m.pt')
+    elif model_name == "yolov5l":
+        model = YOLO('yolov5l.pt')
+    elif model_name == "yolov5x":
+        model = YOLO("yolov5x.pt")
+    # yolo 8
+    elif model_name == "yolov8n":
+        model = YOLO("yolov8n.pt")
+    elif model_name == "yolov8s":
+        model = YOLO("yolov8s.pt")
+    elif model_name == "yolov8m":
+        model = YOLO("yolov8m.pt")
+    elif model_name == "yolov8l":
+        model = YOLO("yolov8l.pt")
+    elif model_name == "yolov8x":
+        model = YOLO("yolov8x.pt")
+
+    # yolo 9
+    elif model_name == "yolov9t":
+        model = YOLO("yolov9t.pt")
+    elif model_name == "yolov9s":
+        model = YOLO("yolov9s.pt")
+    elif model_name == "yolov9m":
+        model = YOLO("yolov9m.pt")
+    elif model_name == "yolov9c":
+        model = YOLO("yolov9c.pt")
+    elif model_name == "yolov9e":
+        model = YOLO("yolov9e.pt")
+
+    
+    # yolo 10
+    elif model_name == "yolov10n":
+        model = YOLO("yolov10n.pt")
+    elif model_name == "yolov10s":
+        model = YOLO("yolov10s.pt")
+    elif model_name == "yolov10m":
+        model = YOLO("yolov10m.pt")
+    elif model_name == "yolov10l":
+        model = YOLO("yolov10l.pt")
+    elif model_name == "yolov10x":
+        model = YOLO("yolov10x.pt")
+        # Yolo 11
+    elif model_name == "yolo11n":
+        model = YOLO("yolo11n.pt")
+    elif model_name == "yolo11s":
+        model = YOLO("yolo11s.pt")
+    elif model_name == "yolo11m":
+        model = YOLO("yolo11m.pt")
+    elif model_name == "yolo11l":
+        model = YOLO("yolo11l.pt")
+    elif model_name == "yolo11x":
+        model = YOLO("yolo11x.pt")
+    else:
+        raise ValueError(f"Unsupported model: {model_name}")
+    return model
+
+
+# Function to calculate latency and response time
+def calculate_latency(start_time, end_time):
+    return end_time - start_time
+
+
+def run_yo_model(image_path, model_name):
+    model = load_yo_model(model_name)
+    start_time = time.time()
+
+    if image_path.startswith('http'):
+        response = requests.get(image_path)
+        image = Image.open(BytesIO(response.content)).convert("RGB")
+    else:
+        image = Image.open(image_path).convert("RGB")
+
+    # Perform inference on the image
+    results = model(image)
+
+    end_time = time.time()
+    # 
+    latency = end_time - start_time
+
+    detected_objects = []
+    total_confidence = 0
+    num_detections = 0
+
+    for result in results:
+        for box in result.boxes:
+            obj_class = result.names[int(box.cls)]
+            confidence = box.conf.item()
+            bbox = box.xyxy.tolist()[0]  # x_min, y_min, x_max, y_max
+
+            # Transform bbox into the required format
+            x_min, y_min, x_max, y_max = bbox
+            detected_objects.append({
+                'label': obj_class,
+                'confidence': confidence,
+                'x': x_min,
+                'y': y_min,
+                'width': x_max - x_min,
+                'height': y_max - y_min
+            })
+            total_confidence += confidence
+            num_detections += 1
+
+    # Calculate accuracy
+    overall_accuracy = total_confidence / num_detections if num_detections > 0 else 0
+
+    # Get dynamic power consumption from Nvidia GPU
+    power_watts = get_power_usage_nvidia()
+
+    # Calculate energy consumption in joules
+    energy_required = calculate_energy(power_watts, latency)
+
+    # Get image size in bits
+    image_size_bits = get_image_size_in_bits(image_path)
+    throughput_result = float(f"{image_size_bits / latency if latency > 0 else 0:.2f}")
+    throughput = throughput_result / 1000
+
+    return detected_objects, latency, overall_accuracy, throughput, energy_required, power_watts
 
 
 # Function to run the model and perform object detection
@@ -201,117 +336,12 @@ def run_tf_model(image_path, model_name):
     return detected_objects, latency, accuracy, throughput, energy_required, power_watts
 
 
-def load_yo_model(model_name):
-    if model_name == "yolov5n":
-        model = YOLO("yolov5s.pt")
-    elif model_name == "yolov5s":
-        model = YOLO('yolov5s.pt')
-    elif model_name == "yolov5m":
-        model = YOLO('yolov5m.pt')
-    elif model_name == "yolov5l":
-        model = YOLO('yolov5l.pt')
-    elif model_name == "yolov5x":
-        model = YOLO("yolov5x.pt")
-    # yolo 8
-    elif model_name == "yolov8n":
-        model = YOLO("yolov8n.pt")
-    elif model_name == "yolov8s":
-        model = YOLO("yolov8s.pt")
-    elif model_name == "yolov8m":
-        model = YOLO("yolov8m.pt")
-    elif model_name == "yolov8l":
-        model = YOLO("yolov8l.pt")
-    elif model_name == "yolov8x":
-        model = YOLO("yolov8x.pt")
-    # yolo 10
-    elif model_name == "yolov10n":
-        model = YOLO("yolov10n.pt")
-    elif model_name == "yolov10s":
-        model = YOLO("yolov10s.pt")
-    elif model_name == "yolov10m":
-        model = YOLO("yolov10m.pt")
-    elif model_name == "yolov10l":
-        model = YOLO("yolov10l.pt")
-    elif model_name == "yolov10x":
-        model = YOLO("yolov10x.pt")
-        # Yolo 11
-    elif model_name == "yolo11n":
-        model = YOLO("yolo11n.pt")
-    elif model_name == "yolo11s":
-        model = YOLO("yolo11s.pt")
-    elif model_name == "yolo11m":
-        model = YOLO("yolo11m.pt")
-    elif model_name == "yolo11l":
-        model = YOLO("yolo11l.pt")
-    elif model_name == "yolo11x":
-        model = YOLO("yolo11x.pt")
-    else:
-        raise ValueError(f"Unsupported model: {model_name}")
-    return model
-
-def run_yo_model(image_path, model_name):
-    model = load_yo_model(model_name)
-    start_time = time.time()
-
-    if image_path.startswith('http'):
-        response = requests.get(image_path)
-        image = Image.open(BytesIO(response.content)).convert("RGB")
-    else:
-        image = Image.open(image_path).convert("RGB")
-
-    # Perform inference on the image
-    results = model(image)
-
-    end_time = time.time()
-    # 
-    latency = end_time - start_time
-
-    detected_objects = []
-    total_confidence = 0
-    num_detections = 0
-
-    for result in results:
-        for box in result.boxes:
-            obj_class = result.names[int(box.cls)]
-            confidence = box.conf.item()
-            bbox = box.xyxy.tolist()[0]  # x_min, y_min, x_max, y_max
-
-            # Transform bbox into the required format
-            x_min, y_min, x_max, y_max = bbox
-            detected_objects.append({
-                'label': obj_class,
-                'confidence': confidence,
-                'x': x_min,
-                'y': y_min,
-                'width': x_max - x_min,
-                'height': y_max - y_min
-            })
-            total_confidence += confidence
-            num_detections += 1
-
-    # Calculate accuracy
-    overall_accuracy = total_confidence / num_detections if num_detections > 0 else 0
-
-    # Get dynamic power consumption from Nvidia GPU
-    power_watts = get_power_usage_nvidia()
-
-    # Calculate energy consumption in joules
-    energy_required = calculate_energy(power_watts, latency)
-
-    # Get image size in bits
-    image_size_bits = get_image_size_in_bits(image_path)
-    throughput_result = float(f"{image_size_bits / latency if latency > 0 else 0:.2f}")
-    throughput = throughput_result / 1000
-
-    return detected_objects, latency, overall_accuracy, throughput, energy_required, power_watts
-
-# Function to calculate latency and response time
-def calculate_latency(start_time, end_time):
-    return end_time - start_time
-
 # Function to run the model and perform object detection
 def run_model(image_path, model_name="fasterrcnn"):
-    if model_name[:2] == 'tf':
+    if model_name[:2] == 'sg':
+        model_ = model_name[3::]
+        return run_segmentation(image_path=image_path,model_name=model_)
+    elif model_name[:2] == 'tf':
         model_ = model_name[3::]
         return run_tf_model(image_path=image_path,model_name=model_)
     elif model_name[:2] == 'yo':
@@ -384,3 +414,14 @@ def run_model(image_path, model_name="fasterrcnn"):
 
     return detected_objects, latency, accuracy, throughput, energy_required, power_watts
 
+
+# if __name__ == "__main__":
+#     image_path = "ppc-0603.jpeg"  # Replace with your image path or URL
+#     model_name = 'ssd_resnet34'
+#     results, latency, overall_accuracy, throughput, energy_required, power_watts = run_model(image_path, model_name)
+#     # print(f"Detected objects: {results}")
+#     print(f"Latency: {latency:.2f} seconds")
+#     print(f"Accuracy: {overall_accuracy:.2f}")
+#     print(f"throughput: {throughput:.2f}")
+#     print(f"energy_required: {energy_required:.2f}")
+#     print(f"power_watts: {power_watts:.2f}")
